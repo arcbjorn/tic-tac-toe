9ea0fc558b7abd917e41109ae65d000d4948a519
Arc <arky.happ@gmail.com>
2020-05-18 19:48:39 +0300

    Updates

---
+++ precache-manifest.ba99f71951f42479d5adacb863aa7951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90445b7455a54ee335d9b0e31466bf27",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "04bb5542779a8dfb82da",
+    "url": "/tic-tac-toe/static/css/main.80e2af0a.chunk.css"
+  },
+  {
+    "revision": "5bb3f7252f5571199d46",
+    "url": "/tic-tac-toe/static/js/2.d1c3c4e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.d1c3c4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04bb5542779a8dfb82da",
+    "url": "/tic-tac-toe/static/js/main.da067d45.chunk.js"
+  },
+  {
+    "revision": "67093bcd19aa82d2fa67",
+    "url": "/tic-tac-toe/static/js/runtime-main.29e8e2be.js"
+  }
+]);
